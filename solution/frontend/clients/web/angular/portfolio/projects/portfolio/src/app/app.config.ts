5a95999646f50be5aca3257000a1b93fa0ff8ed6
mindarchitect <andriy.lavrusha@gmail.com>
2025-11-15 23:54:31 +0100

    FIrst commit

--- solution/frontend/clients/web/angular/portfolio/projects/portfolio/src/app/app.config.ts
+++ solution/frontend/clients/web/angular/portfolio/projects/portfolio/src/app/app.config.ts
@@ -3,12 +3,24 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideTranslateService} from '@ngx-translate/core';
+import {provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideZoneChangeDetection({eventCoalescing: true}),
         provideRouter(routes),
-        provideClientHydration(withEventReplay())
+        provideClientHydration(withEventReplay()),
+        provideHttpClient(withFetch()),
+        provideTranslateService({
+            loader: provideTranslateHttpLoader({
+                prefix: '/assets/i18n/',
+                suffix: '.json'
+            }),
+            fallbackLang: 'en',
+            lang: 'en'
+        })
     ]
 };
--- solution/frontend/clients/web/angular/portfolio/projects/portfolio/src/app/components/application/application.ts
+++ solution/frontend/clients/web/angular/portfolio/projects/portfolio/src/app/components/application/application.ts
@@ -1,12 +1,15 @@
 import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NgTemplateOutlet} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, NgTemplateOutlet, TranslatePipe],
     templateUrl: './application.html',
     styleUrl: './application.scss'
 })
 export class Application {
     protected readonly title = signal('portfolio');
+    protected readonly currentYear = signal(new Date().getFullYear());
 }
