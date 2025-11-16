59dbfd06c994613d520ce693e8696f567775e711
mindarchitect <andriy.lavrusha@gmail.com>
2025-11-16 16:32:19 +0100

    WIP

---
+++ solution/backend/services/projects/src/test/java/com/andriylavrusha/portfolio/solution/backend/services/projects/ProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriylavrusha.portfolio.solution.backend.services.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ solution/backend/services/test/src/test/java/com/andriylavrusha/portfolio/solution/backend/services/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriylavrusha.portfolio.solution.backend.services.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
