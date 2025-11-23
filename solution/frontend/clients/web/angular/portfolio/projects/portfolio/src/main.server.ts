599356239f7281a22e08625d25242a8d1cac175b
mindarchitect <andriy.lavrusha@gmail.com>
2025-11-23 08:22:07 +0100

    WIP

--- solution/frontend/clients/web/angular/portfolio/projects/portfolio/src/main.server.ts
+++ solution/frontend/clients/web/angular/portfolio/projects/portfolio/src/main.server.ts
@@ -1,8 +1,8 @@
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
+import { Application } from './app/components/application/application';
 import { config } from './app/app.config.server';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
+    bootstrapApplication(Application, config, context);
 
 export default bootstrap;
